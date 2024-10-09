f201a131479af9554e25c4b73b0796ee5ab9a5ed
jkubala4454 <63329541+jkubala4454@users.noreply.github.com>
2024-10-09 13:54:02 -0500

    Add files via upload

--- attendance/includes/config.php
+++ attendance/includes/config.php
@@ -1,4 +1,8 @@
 <?php
-// Define the root path for the project
-define('ROOT_PATH', realpath(dirname(__FILE__) . '/..')); // Go up one directory from the 'includes' folder
+// config.php - Configuration settings
+
+// Define the root path for the project if not already defined
+if (!defined('ROOT_PATH')) {
+    define('ROOT_PATH', realpath(dirname(__FILE__) . '/..')); // Go up one directory from the 'includes' folder
+}
 ?>
